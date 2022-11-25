417439f5e03f28f7fe8f4f13fc202704da34b6a9
hyunjungjeon <sksznddl574@gmail.com>
2022-11-25 13:54:55 +0900

    feature : #8 플레이타임 조회 서비스 생성

--- src/main/java/com/purpleprint/network/purpleprintemailbatch/user/query/service/UserService.java
+++ src/main/java/com/purpleprint/network/purpleprintemailbatch/user/query/service/UserService.java
@@ -1,17 +1,26 @@
 package com.purpleprint.network.purpleprintemailbatch.user.query.service;
 
 import com.purpleprint.network.purpleprintemailbatch.analysis.command.domain.model.Analysis;
-import com.purpleprint.network.purpleprintemailbatch.character.command.domain.model.Character;
 import com.purpleprint.network.purpleprintemailbatch.character.command.domain.model.CharacterFile;
 import com.purpleprint.network.purpleprintemailbatch.user.command.application.dto.PlayFriendDTO;
 import com.purpleprint.network.purpleprintemailbatch.user.command.domain.domain.Child;
+import com.purpleprint.network.purpleprintemailbatch.user.command.domain.domain.Login;
+import com.purpleprint.network.purpleprintemailbatch.user.command.domain.domain.Logout;
 import com.purpleprint.network.purpleprintemailbatch.user.command.domain.repository.ChildRepository;
+import com.purpleprint.network.purpleprintemailbatch.user.command.domain.repository.LoginRepository;
+import com.purpleprint.network.purpleprintemailbatch.user.command.domain.repository.LogoutRepository;
 import com.purpleprint.network.purpleprintemailbatch.user.command.domain.service.AwnerCharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * <pre>
@@ -32,12 +41,17 @@ import java.util.List;
 public class UserService {
 
     private final ChildRepository childRepository;
+    private final LoginRepository loginRepository;
+    private final LogoutRepository logoutRepository;
     private final AwnerCharacterService awnerCharacterService;
 
     @Autowired
-    public UserService(ChildRepository childRepository, AwnerCharacterService awnerCharacterService) {
+    public UserService(ChildRepository childRepository, AwnerCharacterService awnerCharacterService,
+                       LoginRepository loginRepository, LogoutRepository logoutRepository) {
         this.childRepository = childRepository;
         this.awnerCharacterService = awnerCharacterService;
+        this.loginRepository = loginRepository;
+        this.logoutRepository = logoutRepository;
     }
 
     public Child selectRecipient(int recipient) {
@@ -104,5 +118,62 @@ public class UserService {
         }
 
         return playFriendList;
+    }
+
+    public String selectPlayTime(Integer id) {
+
+        String t1 = LocalDate.now() + " 00:00:00";
+        String t2 = LocalDate.now() + " 23:59:59";
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        Date date1 = null;
+        Date date2 = null;
+
+        try {
+            date1 = formatter.parse(t1);
+            date2 = formatter.parse(t2);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        List<Login> loginList = loginRepository.findAllByChildIdAndLoginAtBetweenOrderByIdAsc(id, date1, date2);
+
+        long playtime = 0;
+        String alpha = "";
+        for(Login login : loginList) {
+            Logout logout = logoutRepository.findByLoginId(login.getId());
+            if(logout == null) {
+                alpha = "α";
+                break;
+            }
+            System.out.println(playtime);
+            System.out.println( logout.getLogoutAt().getTime() - login.getLoginAt().getTime());
+            playtime = playtime + logout.getLogoutAt().getTime() - login.getLoginAt().getTime();
+
+            System.out.println(playtime);
+        }
+
+        String playTime = "";
+        int minute = 0;
+
+
+        if((playtime / (60 * 60 * 1000)) != 0) {
+            playTime = playTime + (playtime / (60 * 60 * 1000)) + "시간 ";
+        }
+
+        if((playtime / (60 * 1000)) != 0) {
+            playTime = playTime + ((playtime / (60 * 1000)) -  (playtime / (60 * 60 * 1000) * 60)) + "분 ";
+        }
+
+        if((playtime / 1000) != 0) {
+            playTime = playTime + ((playtime / 1000) -(playtime / (60 * 1000) * 60))  + "초 ";
+        }
+
+        if(alpha.equals("α")) {
+            playTime = playTime + "+ α(게임 플레이 중)";
+        }
+
+        return playTime;
     }
 }

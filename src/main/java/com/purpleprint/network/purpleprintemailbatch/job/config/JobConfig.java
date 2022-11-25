ce6cc95acca29d83346674f989206ca44c2846e5
hyunjungjeon <sksznddl574@gmail.com>
2022-11-25 20:30:36 +0900

    feature : #9 분석 메일 전송 및 메일 전송 여부 변경 job 생성

--- src/main/java/com/purpleprint/network/purpleprintemailbatch/job/config/JobConfig.java
+++ src/main/java/com/purpleprint/network/purpleprintemailbatch/job/config/JobConfig.java
@@ -4,6 +4,8 @@ import com.purpleprint.network.purpleprintemailbatch.analysis.command.query.serv
 import com.purpleprint.network.purpleprintemailbatch.analysis.command.domain.model.Analysis;
 import com.purpleprint.network.purpleprintemailbatch.heart.command.application.dto.RecipientDTO;
 import com.purpleprint.network.purpleprintemailbatch.heart.query.service.HeartService;
+import com.purpleprint.network.purpleprintemailbatch.job.dto.MailDTO;
+import com.purpleprint.network.purpleprintemailbatch.mail.service.MailService;
 import com.purpleprint.network.purpleprintemailbatch.user.command.application.dto.PlayFriendDTO;
 import com.purpleprint.network.purpleprintemailbatch.user.command.domain.domain.User;
 import com.purpleprint.network.purpleprintemailbatch.user.query.service.UserService;
@@ -16,7 +18,10 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.mail.MessagingException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -43,29 +48,40 @@ public class JobConfig {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final AnalysisService analysisService;
+    private final com.purpleprint.network.purpleprintemailbatch.analysis.command.application.service.AnalysisService analysisService2;
     private final HeartService heartService;
     private final UserService userService;
+    private final MailService mailService;
 
     @Value("${job.name}")
     private String BATCH_NAME;
 
-    private List<Analysis> analysisList;
+    private List<Analysis> analysisList = new ArrayList<>();
+    private List<MailDTO> mailList = new ArrayList<>();
 
-    public JobConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, AnalysisService analysisService, HeartService heartService, UserService userService) {
+    public JobConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, AnalysisService analysisService,
+                     HeartService heartService, UserService userService, MailService mailService,
+                     com.purpleprint.network.purpleprintemailbatch.analysis.command.application.service.AnalysisService analysisService2) {
         this.jobBuilderFactory = jobBuilderFactory;
         this.stepBuilderFactory = stepBuilderFactory;
         this.analysisService = analysisService;
+        this.analysisService2 = analysisService2;
         this.heartService = heartService;
         this.userService = userService;
+        this.mailService = mailService;
     }
 
     @Bean
     public Job job() {
         return jobBuilderFactory.get(BATCH_NAME)
                 .start(step1())
+                .next(step1())
                 .next(step2())
+                .next(step3())
+                .next(step4())
                 .build();
     }
+
 
     @Bean
     public Step step1() {
@@ -84,10 +100,13 @@ public class JobConfig {
         return stepBuilderFactory.get(BATCH_NAME + "step 2")
                 .tasklet((stepContribution, chunkContext) -> {
                     log.debug("============ 분석 메일 전송 데이터 호출 =============");
+                    mailList = new ArrayList<>();
+                    for(Analysis analysis : analysisList) {
 
-                    analysisList.forEach(analysis -> {
+                        MailDTO mail = new MailDTO();
+
                         // 하트 준 사람 호출
-                        List<RecipientDTO> giveHeartFriendList = heartService.selectHeartList(analysis.getChild());
+                        List<RecipientDTO> giveHeartFriendList = heartService.selectHeartList(analysis.getChild(), analysis.getAnalysisAt());
 
                         //함께 플레이한 사람 호출
                         List<PlayFriendDTO> playFriendList = userService.selectFriendList(analysis);
@@ -96,10 +115,52 @@ public class JobConfig {
                         User parents = analysis.getChild().getUser();
 
                         //플레이타임 조회
-                        String playTime =userService.selectPlayTime(analysis.getChild().getId());
+                        String playTime =userService.selectPlayTime(analysis.getChild().getId(), analysis.getAnalysisAt());
 
+                        mail.setChildName(analysis.getChild().getName());
+                        mail.setParentsMail(parents.getEmail());
+                        mail.setRecipientList(giveHeartFriendList);
+                        mail.setPlayFriendList(playFriendList);
+                        mail.setPlayPlace(analysis.getPlayPlace());
+                        mail.setConCurrentFriend(analysis.getConcurrentChild());
+                        mail.setPlayTime(playTime);
+                        mail.setGiveHeartCount(giveHeartFriendList.size());
+                        mail.setComment(analysis.getFinalFeedback());
+                        System.out.println(mail);
+                        mailList.add(mail);
+                    }
+
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get(BATCH_NAME + "step 3")
+                .tasklet((stepContribution, chunkContext) -> {
+                    log.debug("============ 분석 메일 전송 및 전송  =============");
+
+                    mailList.forEach(mail -> {
+                        try {
+                            mailService.sendMail(mail);
+                        } catch (MessagingException e) {
+                            throw new RuntimeException(e);
+                        }
                     });
 
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Step step4() {
+        return stepBuilderFactory.get(BATCH_NAME + "step 4")
+                .tasklet((stepBuilderFactory, chunkContext) -> {
+                    log.debug("============ 전송 완료여부 변경 =============");
+
+                    for(Analysis analysis : analysisList) {
+                        analysisService2.updateAnalysis(analysis);
+                    }
                     return RepeatStatus.FINISHED;
                 }).build();
     }
